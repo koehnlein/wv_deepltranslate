0004c43c0d60a26fc553b197d24c8ae414d4b989
Albrecht Köhnlein <ak@koehnlein.eu>
2024-02-21 13:29:46 +0100

    Fix icon paths

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 return [
     'apps-pagetree-folder-contains-glossary' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:wv_deepltranslate/Resources/Public/deepl.svg',
+        'source' => 'EXT:wv_deepltranslate/Resources/Public/Icons/deepl.svg',
     ],
     'actions-localize-deepl' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:wv_deepltranslate/Resources/Public/actions-localize-deepl.svg',
+        'source' => 'EXT:wv_deepltranslate/Resources/Public/Icons/actions-localize-deepl.svg',
     ],
     'deepl-grey-logo' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
